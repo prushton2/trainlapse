137fdebb5ce1ccdb9c30d03a9920c43e5be1352b
prushton2 <peterrushton418@gmail.com>
2025-03-29 13:28:47 -0400

    added train icoins

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css'
 import 'leaflet/dist/leaflet.css'
-import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
+import trainIcon from './assets/train.svg'
+import { MapContainer, TileLayer, Marker, Popup, Polyline, CircleMarker} from 'react-leaflet';
+import { Icon } from 'leaflet';
 import { polyline, getLines, getColor } from './Lines';
 import { JSX, useEffect, useState } from 'react';
 import { getLatestTrainData } from './API';
@@ -11,6 +13,13 @@ export function App() {
   const [sliderMax, setSliderMax] = useState<number>(0);
   const [trainInfo, setTrainInfo] = useState<TrainInfoResponse | null>(null);
   const [trainData, setTrainData] = useState<JSX.Element[]>([]);
+
+  let icon: Icon = new Icon({
+    iconUrl: trainIcon,
+    iconSize: [25, 25], // size of the icon
+    iconAnchor: [12.5, 12.5], // point of the icon which will correspond to marker's location
+    popupAnchor: [0, -12.5] // point from which the popup should open relative to the iconAnchor
+  });
 
   function renderLines(): JSX.Element[] {
     let element: JSX.Element[] = []
@@ -29,33 +38,31 @@ export function App() {
       setSliderMax(trainInfo.size)
       return []
     }
+
     let element: JSX.Element[] = []
-    console.log(trainInfo.elements[slider].timestamp)
     trainInfo.elements[slider].data.forEach((e) => {
-      element.push(
-        <Marker key={element.length} position={[e.latitude, e.longitude]}>
-          <Popup>
-            <h2>{e.label}</h2>
-            <p>Speed: {e.speed}</p>
-            <p>Headsign: {e.headsign}</p>
-          </Popup>
-        </Marker>
+      element.push(<>
+          <Marker icon={icon} key={element.length} position={[e.latitude, e.longitude]}>
+            <img src={trainIcon} alt="React Logo" />
+            <Popup>
+              <h2>{e.label}</h2>
+              <p>Speed: {e.speed}</p>
+              <p>Headsign: {e.headsign}</p>
+            </Popup>
+          </Marker>
+          <CircleMarker key={element.length} center={[e.latitude, e.longitude]} radius={15} color={getColor("CR-all")} fillColor={getColor("CR-all")} fillOpacity={1}/>
+        </>
       )
     })
     return element;
   }
 
   useEffect(() => {
-    console.log(`Slider value changed: ${slider}`)
     async function run() {
       setTrainData(await renderTrains())
     }
     run()
   }, [slider])
-
-  useEffect(() => {
-    console.log(`App mounted`)
-  }, [trainData])
 
   return <>
       <div className="slidecontainer">
