7ade57e673760828f10f4004d63acb1f30f7ab10
prushton2 <peterrushton418@gmail.com>
2025-03-29 22:14:50 -0400

    added time

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,13 @@ export function App() {
   const [slider, setSlider] = useState<number>(0);
   const [sliderMax, setSliderMax] = useState<number>(0);
   const [trainInfo, setTrainInfo] = useState<TrainInfoResponse | null>(null);
-  const [trainData, setTrainData] = useState<JSX.Element[]>([]);
+  const [trainHTML, setTrainHTML] = useState<JSX.Element[]>([]);
 
   let icon: Icon = new Icon({
     iconUrl: trainIcon,
-    iconSize: [25, 25], // size of the icon
-    iconAnchor: [12.5, 12.5], // point of the icon which will correspond to marker's location
-    popupAnchor: [0, -12.5] // point from which the popup should open relative to the iconAnchor
+    iconSize: [25, 25],
+    iconAnchor: [12.5, 12.5],
+    popupAnchor: [0, -12.5]
   });
 
   function renderLines(): JSX.Element[] {
@@ -36,14 +36,14 @@ export function App() {
     if(trainInfo === null) {
       let trainInfo: TrainInfoResponse = await getLatestTrainData();
       setTrainInfo(trainInfo);
-      setSliderMax(trainInfo.size)
+      setSliderMax(trainInfo.size-1)
       return []
     }
 
     let element: JSX.Element[] = []
     trainInfo.elements[slider].data.forEach((e) => {
       element.push(<>
-          <Marker icon={icon} key={element.length} position={[e.latitude, e.longitude]}>
+          <Marker icon={icon} key={element.length*2} position={[e.latitude, e.longitude]}>
             <img src={trainIcon} alt="Train" />
             <Popup>
               <h2>{e.label} ({getVehicleType(parseInt(e.label))})</h2>
@@ -51,29 +51,50 @@ export function App() {
               <p>Headsign: {e.headsign}</p>
             </Popup>
           </Marker>
-          <CircleMarker key={element.length} center={[e.latitude, e.longitude]} radius={15} color={getColor("CR-all")} fillColor={getColor("CR-all")} fillOpacity={1}/>
+          <CircleMarker key={element.length*2+1} center={[e.latitude, e.longitude]} radius={15} color={getColor("CR-all")} fillColor={getColor("CR-all")} fillOpacity={1}/>
         </>
       )
     })
     return element;
   }
 
+  function convertTimestampToDate(timestamp: number): String {
+    const date = new Date(timestamp * 1000);
+    return date.toLocaleString('en-US', {
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false
+    });
+  }
+  
   useEffect(() => {
     async function run() {
-      setTrainData(await renderTrains())
+      setTrainHTML(await renderTrains())
     }
     run()
   }, [slider])
 
   return <>
-      <div className="slidecontainer">
+      <div className="slidecontainer" style={{ height: "5vh" }}>
         <input type="range" min="1" max={sliderMax} className="slider" onChange={(e) => setSlider(e.target.valueAsNumber)}/>
       </div>
-      <MapContainer center={[42.36041830331139, -71.0580009624248]} zoom={13} style={{ height:"1080px",backgroundColor:"black" }}>
+      
+      <div style={{ position: "absolute", right: "1vh", top: "4vh", color: "white" }}>
+        <h3>Time: {trainInfo == null ? 0 : convertTimestampToDate(trainInfo.elements[slider].timestamp)}</h3>
+      </div>
+      
+      {/* <div className="slidecontainer" style={{ position: "absolute", right: "1vh", top: "10vh", height: "5vh", width: "18%"}}>
+        <input type="range" min="1" max="10" className="slider"/>
+      </div> */}
+
+
+      <MapContainer center={[42.36041830331139, -71.0580009624248]} zoom={13} style={{ height: "93vh", width: "80%", backgroundColor:"black" }}>
         <TileLayer url="https://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png" />
         {renderLines()}
-        {trainData}
-      </MapContainer>    
+        {trainHTML}
+      </MapContainer>
+      
     </>
 }
 
